acc337a9b22df1ce7ce14769e909b2e01d93ef53
hotio <hotio@protonmail.com>
2019-11-16 10:04:28 +0100

    [CI SKIP] add failsafe

--- update.sh
+++ update.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 version=$(curl -fsSL "https://api.github.com/repos/nzbget/nzbget/releases" | jq -r .[0].name | sed s/v//g)
+[[ -z ${version} ]] && exit
 find . -type f -name '*.Dockerfile' -exec sed -i "s/ARG NZBGET_VERSION=.*$/ARG NZBGET_VERSION=${version}/g" {} \;
 find . -type f -name '*.Dockerfile' -exec sed -i "s/ARG NZBGET_VERSION_SHORT=.*$/ARG NZBGET_VERSION_SHORT=${version//-testing/}/g" {} \;
 sed -i "s/{TAG_VERSION=.*}$/{TAG_VERSION=${version//-testing/}}/g" .drone.yml
