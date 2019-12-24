81f896e8c8651c5a4932c556e79de76908531e64
hotio <hotio@protonmail.com>
2019-12-24 10:22:25 +0100

    [CI SKIP] exit 1 in update.sh

--- update.sh
+++ update.sh
@@ -46,7 +46,7 @@ elif [[ ${1} == "checkdigests" ]]; then
     digest=$(echo "${manifest}" | jq -r '.manifests[] | select (.platform.architecture == "arm64" and .platform.os == "linux").digest') && sed -i "s#FROM .*\$#FROM ${image}@${digest}#g" ./linux-arm64.Dockerfile && echo "${digest}"
 else
     version=$(curl -fsSL "https://api.github.com/repos/nzbget/nzbget/releases" | jq -r .[0].name | sed s/v//g)
-    [[ -z ${version} ]] && exit
+    [[ -z ${version} ]] && exit 1
     find . -type f -name '*.Dockerfile' -exec sed -i "s/ARG NZBGET_VERSION=.*$/ARG NZBGET_VERSION=${version}/g" {} \;
     find . -type f -name '*.Dockerfile' -exec sed -i "s/ARG NZBGET_VERSION_SHORT=.*$/ARG NZBGET_VERSION_SHORT=${version//-testing/}/g" {} \;
     sed -i "s/{TAG_VERSION=.*}$/{TAG_VERSION=${version//-testing/}}/g" .drone.yml
